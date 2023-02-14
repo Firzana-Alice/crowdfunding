885a4ae8a98de6bdf5d6c3a30c05ad0a636ef046
Firzana-Alice <114482681+Firzana-Alice@users.noreply.github.com>
2023-02-14 20:27:26 +0800

    App set up
    All code from class (Tues)

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,21 @@
+//Data
+
+
+
+
+import { Link } from "react-router-dom";
+
+function ProjectCard(props) {
+  const { projectData } = props;
+
+  return (
+    <div>
+      <Link to="/project">
+        <img src={projectData.image} />
+        <h3>{projectData.title}</h3>
+      </Link>
+    </div>
+  );
+}
+
+export default ProjectCard;
